f5e112cc4f15930d7663b1f6563a66581b6e4cb0
Pedro Nascimento <pnascimento@gmail.com>
2012-04-09 22:52:08 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wargame2::Application.config.secret_token = '5a32a2f319c9cb423f40a32a0fdf08e2106175be7e08e2d3e3126a9e6845559a3f6a1c3dd15fe4e8aad33b314814ba44ffcd46f7fc5ef977841a9f634344335c'
